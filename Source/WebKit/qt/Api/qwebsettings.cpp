94d8c9874eff28332c8febac279764d9e4ac4477
commit-queue <commit-queue@webkit.org>
2012-11-30 08:48:28 +0000

    [CSSRegions] when WebKit uses V8, there should be a single variable to store if the CSS Regions feature is enabled https://bugs.webkit.org/show_bug.cgi?id=101192
    Patch by Mihai Maerean <mmaerean@adobe.com> on 2012-11-30
    Reviewed by Hajime Morita.
    
    Removed the CSS Regions flag in Settings and switched to using the new flag I have added in RuntimeEnabledFeatures.
    
    Tests: No new tests because there is no functional change.
    
    Source/WebCore:
    
    * WebCore.exp.in:
    * WebCore.vcproj/copyForwardingHeaders.cmd:
    * WebCore.xcodeproj/project.pbxproj:
    * bindings/generic/RuntimeEnabledFeatures.cpp:
    (WebCore):
    * bindings/generic/RuntimeEnabledFeatures.h:
    (RuntimeEnabledFeatures):
    (WebCore::RuntimeEnabledFeatures::setCSSRegionsEnabled):
    (WebCore::RuntimeEnabledFeatures::cssRegionsEnabled):
    * dom/Document.cpp:
    (WebCore::Document::cssRegionsEnabled):
    * page/Settings.cpp:
    (WebCore::Settings::Settings):
    * page/Settings.h:
    (Settings):
    
    Source/WebKit/chromium:
    
    * public/WebRuntimeFeatures.h:
    (WebRuntimeFeatures):
    * public/WebSettings.h: moved setExperimentalCSSRegionsEnabled to the bottom of the interface and made it a
    no-op.  After this patch lands and Chromium's webkit_rev is increased, we'll remove any callers in Chromium. Once
    the Chromium callers are gone and WebKit's chromium_rev is increased, we'll remove the deprecated API.
    * src/WebRuntimeFeatures.cpp:
    (WebKit::WebRuntimeFeatures::enableCSSRegions):
    (WebKit):
    (WebKit::WebRuntimeFeatures::isCSSRegionsEnabled):
    * src/WebSettingsImpl.cpp:
    * src/WebSettingsImpl.h:
    (WebSettingsImpl):
    
    Source/WebKit/efl:
    
    * WebCoreSupport/DumpRenderTreeSupportEfl.cpp:
    (DumpRenderTreeSupportEfl::setCSSRegionsEnabled):
    * ewk/ewk_view.cpp:
    (_ewk_view_priv_new):
    
    Source/WebKit/gtk:
    
    * WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
    (DumpRenderTreeSupportGtk::setCSSRegionsEnabled):
    
    Source/WebKit/mac:
    
    * WebView/WebView.mm:
    (-[WebView _preferencesChanged:]):
    
    Source/WebKit/qt:
    
    * Api/qwebsettings.cpp:
    (QWebSettingsPrivate::apply):
    
    Source/WebKit/win:
    
    * WebView.cpp:
    (WebView::notifyPreferencesChanged):
    
    Source/WebKit2:
    
    * WebProcess/InjectedBundle/InjectedBundle.cpp:
    (WebKit::InjectedBundle::overrideBoolPreferenceForTestRunner):
    (WebKit::InjectedBundle::setCSSRegionsEnabled):
    (WebKit):
    * WebProcess/InjectedBundle/InjectedBundle.h:
    (InjectedBundle):
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::updatePreferences):
    
    Tools:
    
    * DumpRenderTree/chromium/WebPreferences.cpp:
    (WebPreferences::applyTo):
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@136210 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Conflicts:
    	Source/WebKit/qt/Api/qwebsettings.cpp

--- Source/WebKit/qt/Api/qwebsettings.cpp
+++ Source/WebKit/qt/Api/qwebsettings.cpp
@@ -34,6 +34,8 @@
 #include "InitWebCoreQt.h"
 #include "Page.h"
 #include "PageCache.h"
+#include "PluginDatabase.h"
+#include "RuntimeEnabledFeatures.h"
 #include "Settings.h"
 #include "KURL.h"
 #include "IconDatabase.h"
@@ -173,7 +175,7 @@ void QWebSettingsPrivate::apply()
 
         value = attributes.value(QWebSettings::CSSRegionsEnabled,
                                  global->attributes.value(QWebSettings::CSSRegionsEnabled));
-        settings->setCSSRegionsEnabled(value);
+        WebCore::RuntimeEnabledFeatures::setCSSRegionsEnabled(value);
         value = attributes.value(QWebSettings::CSSGridLayoutEnabled,
                                  global->attributes.value(QWebSettings::CSSGridLayoutEnabled));
         settings->setCSSGridLayoutEnabled(value);

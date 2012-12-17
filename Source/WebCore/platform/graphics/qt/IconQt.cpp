c76566644b8e5c764e25f58a2d41a3d83ebc530b
Allan Sandfeld Jensen <allan.jensen@digia.com>
2012-12-17 19:14:35 +0100

    Revert "[Qt] Implement the mimetype icon methods"
    This reverts commit 47584b6cabbc0d9045afd92bd2c9c310c6735acb.

--- Source/WebCore/platform/graphics/qt/IconQt.cpp
+++ Source/WebCore/platform/graphics/qt/IconQt.cpp
@@ -24,7 +24,6 @@
 #include "GraphicsContext.h"
 #include "IntRect.h"
 #include "NotImplemented.h"
-#include <QMimeDatabase>
 #include <wtf/text/WTFString.h>
 
 namespace WebCore {
@@ -38,49 +37,16 @@ Icon::~Icon()
 }
 
 // FIXME: Move the code to ChromeClient::iconForFiles().
-PassRefPtr<Icon> Icon::createIconForFiles(const Vector<String>& filenames)
+PassRefPtr<Icon> Icon::createIconForFiles(const Vector<String>&)
 {
-    if (filenames.isEmpty())
-        return 0;
-
-    QMimeType mimeType = QMimeDatabase().mimeTypeForFile(filenames[0], QMimeDatabase::MatchExtension);
-
-    QString iconName = mimeType.iconName();
-    QString genericIconName = mimeType.genericIconName();
-
-    // We try to match one of three cases:
-    // 1. All the files have the same type.
-    // 2. All the files are of the same generic type.
-    // 3. The files are not even of the same generic type.
-    const int count = filenames.size();
-    for (int i = 1; i < count; ++i) {
-        mimeType = QMimeDatabase().mimeTypeForFile(filenames[i], QMimeDatabase::MatchExtension);
-        if (iconName != mimeType.iconName())
-            iconName.clear();
-        if (genericIconName != mimeType.genericIconName()) {
-            genericIconName.clear();
-            break;
-        }
-    }
-
-    // FIXME: By default, only X11 will support themed icons.
-    RefPtr<Icon> icon = adoptRef(new Icon);
-    if (!iconName.isEmpty())
-        icon->m_icon = QIcon::fromTheme(iconName, QIcon::fromTheme(genericIconName));
-    else if (!genericIconName.isEmpty())
-        icon->m_icon = QIcon::fromTheme(genericIconName);
-
-    if (icon->m_icon.isNull())
-        return 0;
-    return icon.release();
+    // FIXME: Should use QMimeType in Qt 5.
+    notImplemented();
+    return 0;
 }
 
-void Icon::paint(GraphicsContext* context, const IntRect& rect)
+void Icon::paint(GraphicsContext*, const IntRect&)
 {
-    if (m_icon.isNull())
-        return;
-
-    m_icon.paint(context->platformContext(), rect);
+    notImplemented();
 }
 
 }

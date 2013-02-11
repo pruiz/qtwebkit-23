91e04e4564a9e913d19196793a14035c7abc6498
Allan Sandfeld Jensen <allan.jensen@digia.com>
2013-02-11 14:07:46 +0100

    Revert "Regression(r129944): Heap-use-after-free in WebCore::computeNonFastScrollableRegion"
    This reverts commit 13b72d858b8861c3ea8be61891d257681f535582.

--- Source/WebCore/rendering/RenderRuby.cpp
+++ Source/WebCore/rendering/RenderRuby.cpp
@@ -184,7 +184,7 @@ void RenderRubyAsInline::addChild(RenderObject* child, RenderObject* beforeChild
     RenderRubyRun* lastRun = lastRubyRun(this);
     if (!lastRun || lastRun->hasRubyText()) {
         lastRun = RenderRubyRun::staticCreateRubyRun(this);
-        RenderInline::addChild(lastRun, beforeChild);
+        RenderInline::addChild(lastRun);
     }
     lastRun->addChild(child);
 }
@@ -291,7 +291,7 @@ void RenderRubyAsBlock::addChild(RenderObject* child, RenderObject* beforeChild)
     RenderRubyRun* lastRun = lastRubyRun(this);
     if (!lastRun || lastRun->hasRubyText()) {
         lastRun = RenderRubyRun::staticCreateRubyRun(this);
-        RenderBlock::addChild(lastRun, beforeChild);
+        RenderBlock::addChild(lastRun);
     }
     lastRun->addChild(child);
 }

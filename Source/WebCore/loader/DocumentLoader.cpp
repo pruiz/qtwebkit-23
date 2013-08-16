27d9250bb6b1cc45ca00c8e5f2c4fcc56526467b
Brady Eidson <beidson@apple.com>
2013-08-16 15:11:07 +0200

    REGRESSION (r130783): Scrolling is broken going back to a cached page from a page that still has outstanding subresources. <rdar://problem/14601124> and https://bugs.webkit.org/show_bug.cgi?id=119416
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Test: http/tests/loading/unfinished-load-back-to-cached-page-callbacks.html
    
    * loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::stopLoading): Always explicitly cancel the Document parser when stopLoading is called.
    
    LayoutTests:
    
    * http/tests/loading/resources/page-with-slow-loading-subresource.html: Added.
    * http/tests/loading/resources/slowimage.php: Added.
    * http/tests/loading/unfinished-load-back-to-cached-page-callbacks-expected.txt:
    * http/tests/loading/unfinished-load-back-to-cached-page-callbacks.html:
    * http/tests/loading/unfinished-main-resource-back-to-cached-page-callbacks-expected.txt: Copied from LayoutTests/http/tests/loading/unfinished-load-back-to-cached-page-callbacks-expected.txt.
    * http/tests/loading/unfinished-main-resource-back-to-cached-page-callbacks.html: Copied from LayoutTests/http/tests/loading/unfinished-load-back-to-cached-page-callbacks.html.
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@153649 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/DocumentLoader.cpp
+++ Source/WebCore/loader/DocumentLoader.cpp
@@ -261,12 +261,6 @@ void DocumentLoader::stopLoading()
     if (m_mainResourceLoader) {
         // Stop the main resource loader and let it send the cancelled message.
         m_mainResourceLoader->cancel();
-    
-        // When cancelling the main resource load, we need to also cancel the Document's parser.
-        // Otherwise cancelling the parser when starting the next page load might result
-        // in unexpected side effects such as erroneous event dispatch. ( http://webkit.org/b/117112 )
-        if (Document* doc = document())
-            doc->cancelParsing();
     } else if (!m_subresourceLoaders.isEmpty())
         // The main resource loader already finished loading. Set the cancelled error on the 
         // document and let the subresourceLoaders send individual cancelled messages below.
@@ -275,6 +269,12 @@ void DocumentLoader::stopLoading()
         // If there are no resource loaders, we need to manufacture a cancelled message.
         // (A back/forward navigation has no resource loaders because its resources are cached.)
         mainReceivedError(frameLoader->cancelledError(m_request));
+
+    // We always need to explicitly cancel the Document's parser when stopping the load.
+    // Otherwise cancelling the parser while starting the next page load might result
+    // in unexpected side effects such as erroneous event dispatch. ( http://webkit.org/b/117112 )
+    if (Document* document = m_frame->document())
+        document->cancelParsing();
     
     stopLoadingSubresources();
     stopLoadingPlugIns();
